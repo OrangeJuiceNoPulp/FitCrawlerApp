f744d127182bf5c27506d81bff8eee836e4c04d5
JoeMarchione <ttjtv@outlook.com>
2025-02-19 20:18:22 -0500

    Added columns to Task table

--- fitness/models.py
+++ fitness/models.py
@@ -9,11 +9,15 @@ class Task(models.Model):
     # PK (id) is automatically handled by Django 
     # (Future models will not include these comments because I am tired of writing them each time.)
     
+    DIFFICULTIES = (('Noob', 'Noob'), ('Easy', 'Easy'), ('Medium', 'Medium'), ('Hard', 'Hard'), ('Expert', 'Expert'))
+
     # Specify columns for the table in the database
     name = models.CharField(max_length=255)
     sets = models.IntegerField()
+    difficulty_score = models.CharField(max_length=31, choices=DIFFICULTIES, default='Medium')
     num_per_set = models.IntegerField()
     days_of_week = models.CharField(max_length=15)
+    end_date = models.DateTimeField(null=True)
     
     user = models.ForeignKey(FitCrawlerUser, on_delete=models.CASCADE)
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
